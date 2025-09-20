2a4e827a6ca59c296a579fc76aecf6c790e4a14f
jonny2025dev <akhrorbek0704@icloud.com>
2025-09-20 21:15:39 +0900

    feat: create checkAuthRoles for testting

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -10,41 +10,41 @@ import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 
-@Resolver() 
+@Resolver()
 export class MemberResolver {
   constructor(private readonly memberService: MemberService) {}
-
+  
   @Mutation(() => Member)
   public async signup(@Args('input') input: MemberInput): Promise<Member> {
-    console.log('Mutation: signup');
-    return this.memberService.signup(input);
+      console.log('Mutation: signup');
+      return this.memberService.signup(input);
   }
 
   @Mutation(() => Member)
   public async login(@Args('input') input: LoginInput): Promise<Member> {
-    console.log('Mutation: login');
-    return this.memberService.login(input);
-  }
-  @UseGuards( AuthGuard)
+      console.log('Mutation: login');
+      return this.memberService.login(input); 
+  }  
+ 
+  @UseGuards(AuthGuard)
   @Mutation(() => String)
   public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<string> {
     console.log('Mutation: updateMember');
     return this.memberService.updateMember();
   }
 
-
-  @UseGuards( AuthGuard)
+  @UseGuards(AuthGuard)
   @Query(() => String)
   public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
     console.log('Query: checkAuth');
-    console.log( "memberNick" , memberNick);
+    console.log('memberNick:', memberNick);
     return `Hi ${memberNick}`;
   }
-
+  
   @Roles(MemberType.USER, MemberType.AGENT)
   @UseGuards(RolesGuard)
   @Query(() => String)
-    public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
+  public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
     console.log('Query: checkAuthRoles');
     return `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
   }
@@ -55,19 +55,20 @@ export class MemberResolver {
     return this.memberService.getMember();
   }
 
-  // Authorization: ADMIN
+  /** ADMIN */
+
   @Roles(MemberType.ADMIN)
   @UseGuards(RolesGuard)
   @Mutation(() => String)
   public async getAllMembersByAdmin(): Promise<string> {
+    console.log('Mutation: getAllMembersByAdmin');
     return this.memberService.getAllMembersByAdmin();
   }
-  
-// Authorization: ADMIN
-@Mutation(() => String)
-public async updateMemberByAdmin(): Promise<string> {
-  console.log('Mutation: updateMemberByAdmin');
-  return this.memberService.updateMemberByAdmin();
-}
 
+  // Authorization: ADMIN
+  @Mutation(() => String)
+  public async updateMemberByAdmin(): Promise<string> {
+    console.log('Mutation: updateMemberByAdmin');
+    return this.memberService.updateMemberByAdmin();
+  }
 }
--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -1,18 +1,18 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
-import { Member } from "../../libs/dto/member/member";
-import { LoginInput, MemberInput } from "../../libs/dto/member/member.input";
-import { MemberStatus } from "../../libs/enums/member.enum";
-import { Message } from "../../libs/enums/common.enum";
-import { AuthService } from "../auth/auth.service";
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { MemberStatus } from '../../libs/enums/member.enum';
+import { Message } from '../../libs/enums/common.enum';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class MemberService {
   constructor(
-    @InjectModel('Member') private readonly memberModel: Model<Member>,
-    private  authService: AuthService,
-  ) {}
+    @InjectModel('Member') private readonly memberModel: Model<Member>, 
+    private authService: AuthService,
+  ) {} 
 
   public async signup(input: MemberInput): Promise<Member> {
     input.memberPassword = await this.authService.hashPassword(input.memberPassword);
@@ -25,30 +25,27 @@ export class MemberService {
       throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
     }
   }
-
-
   
-    public async login(input: LoginInput): Promise<Member> {
-      const { memberNick, memberPassword } = input;
-      const response: Member = await this.memberModel
-        .findOne({ memberNick: memberNick })
-        .select('+memberPassword')
-        .exec();
-    
-      if (!response || response.memberStatus === MemberStatus.DELETE) {
-        throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
-      } else if (response.memberStatus === MemberStatus.BLOCK) {
-        throw new InternalServerErrorException(Message.BLOCKED_USER);
-      }
-    
-      // TODO: Compare passwords
-      const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword);
-      if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
-      response.accessToken = await this.authService.createToken(response);
-    
-      return response;
+  public async login(input: LoginInput): Promise<Member> {
+    const { memberNick, memberPassword } = input;
+    const response: Member = await this.memberModel
+      .findOne({ memberNick: memberNick })
+      .select('+memberPassword')
+      .exec();
+
+    if(!response || response.memberStatus === MemberStatus.DELETE) {
+      throw new InternalServerErrorException(Message.NO_MEMBER_NICK);
+    } else if(response.memberStatus === MemberStatus.BLOCK) {
+      throw new InternalServerErrorException(Message.BLOCKED_USER);
     }
-    
+
+    const isMatch = await this.authService.comparePassword(input.memberPassword, response.memberPassword);
+    if(!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+    response.accessToken = await this.authService.createToken(response);
+
+    return response;
+  }
+
   public async updateMember(): Promise<string> {
     return 'updateMember executed!';
   }
@@ -57,11 +54,11 @@ export class MemberService {
     return 'getMember executed!';
   }
 
-  public async getAllMembersByAdmin(): Promise<string> {
-    return 'getAllMembersByAdmin executed!';
-  }
-
   public async updateMemberByAdmin(): Promise<string> {
     return 'updateMemberByAdmin executed!';
+  }
+
+  public async getAllMembersByAdmin(): Promise<string> {
+    return 'getAllMembersByAdmin executed!';
   }
 }
