efe034a64b117de673b2ab2c5a1ce120d8bc7dbf
jonny2025dev <akhrorbek0704@icloud.com>
2025-10-03 22:47:02 +0900

    feat: create likeService model

---
+++ apps/nestar-api/src/components/like/like.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { LikeModule } from './like.module';
+import { Model } from 'mongoose';
+import { Like } from '../../libs/dto/like/like';
+
+@Injectable()
+export class LikeService {
+    constructor(@InjectModel('Like' ) private readonly likeModel: Model<Like>) {}
+}
