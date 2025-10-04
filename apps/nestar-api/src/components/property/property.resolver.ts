a1a7df16287edaa9f0300c06ce64f83f18cf0a4b
jonny2025dev <akhrorbek0704@icloud.com>
2025-10-04 12:36:16 +0900

    feat: develop likeTargetProperty GraphQL api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -11,6 +11,7 @@ import { MemberType } from '../../libs/enums/memeber.enum';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
+import { AuthGuard } from '../auth/guards/auth.guard';
 
 @Resolver(() => Property)
 export class PropertyResolver {
@@ -72,6 +73,18 @@ public async updateProperty(
    console.log('Query: getAgentProperties');
    return await this.propertyService.getAgentProperties(memberId, input);
  }
+
+
+   @UseGuards(AuthGuard)
+    @Mutation(() => Property)
+    public async likeTargetProperty(
+      @Args('propertyId') input: string,
+      @AuthMember('_id') memberId: ObjectId,
+    ): Promise<Property> {
+      console.log('Mutation: likeTargetProperty');
+      const likeRefId = shapeIntoMongoObjectId(input);
+      return await this.propertyService.likeTargetProperty(memberId, likeRefId);
+    }
   
   /** ADMIN **/
   
--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -12,6 +12,9 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
 import moment from 'moment';
 import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
+import { LikeService } from '../like/like.service';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { LikeGroup } from '../../libs/enums/like.enum';
 
 
 @Injectable()
@@ -19,6 +22,8 @@ export class PropertyService {
   constructor(@InjectModel('Property')private readonly propertyModel: Model<Property>,
     private readonly memberService: MemberService,
     private viewService: ViewService,
+    private likeService: LikeService,
+
   ) {}
 
   public async createProperty(input: PropertyInput): Promise<Property> {
@@ -185,7 +190,28 @@ export class PropertyService {
   
     return result[0];
   }
-  
+   
+   public async likeTargetProperty(memberId: ObjectId, likeRefId: ObjectId): Promise<Property> {
+      const target: Property = await this.propertyModel
+      .findOne({ _id: likeRefId, propertyStatus: PropertyStatus.ACTIVE })
+      .exec();
+      
+      if (!target) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+      
+      const input: LikeInput = {
+        memberId: memberId,
+        likeRefId: likeRefId,
+        likeGroup: LikeGroup.PROPERTY,
+      };
+      
+      // LIKE TOGGLE via Like modules
+      const modifier: number = await this.likeService.toggleLike(input);
+      const result = await this.propertyStatsEditor({ _id: likeRefId, targetKey: 'propertyLikes', modifier });
+      
+      if (!result) throw new InternalServerErrorException(Message.SOMETHING_WENT_WRONG);
+      return result;
+      }
+
   public async getAllPropertiesByAdmin(input: AllPropertiesInquiry): Promise<Properties> {
     const { propertyStatus, propertyLocationList } = input.search;
     const match: T = {};
